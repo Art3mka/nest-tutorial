3d14d2c304153d44aab4c5aee1fe695881879758
Artem Shein <tema34468@gmail.com>
2026-06-16 23:03:10 +0300

    Add documentation to project

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { AllExceptionFilter } from './common/filters/all-exceptions.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -11,6 +12,19 @@ async function bootstrap() {
     app.setGlobalPrefix('api');
     app.useGlobalFilters(new AllExceptionFilter());
     app.useGlobalInterceptors(new ResponseInterceptor());
+
+    const config = new DocumentBuilder()
+        .setTitle('Nest Tutorial API')
+        .setDescription('API description for the Nest Tutorial project')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('/docs', app, document, {
+        jsonDocumentUrl: '/swagger.json',
+        yamlDocumentUrl: '/swagger.yaml',
+    });
 
     await app.listen(process.env.PORT ?? 3000);
 }
--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
     IsInt,
     IsNotEmpty,
@@ -10,19 +11,35 @@ import {
 export class UpdateMovieDto {
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({
+        description: 'The title of the movie',
+        example: 'The Dark Knight',
+    })
     title: string;
 
     @IsInt()
     @IsNotEmpty()
     @Min(1900)
     @Max(new Date().getFullYear())
+    @ApiProperty({
+        description: 'The release year of the movie',
+        example: 2008,
+    })
     releaseYear: number;
 
     @IsArray()
     @IsNotEmpty()
     @IsInt({ each: true })
+    @ApiProperty({
+        description: 'The IDs of the actors in the movie',
+        example: ['1', '2', '3'],
+    })
     actorIds: string[];
 
     @IsString()
+    @ApiPropertyOptional({
+        description: 'The URL of the poster of the movie',
+        example: 'https://example.com/poster.jpg',
+    })
     posterUrl: string;
 }
