9ddb136cce68fecff0a802700c6ba13eaeef44f2
Artem Shein <tema34468@gmail.com>
2026-07-10 22:20:06 +0300

    Implement WebSocket functionality in ChatGateway and add message creation logic in ChatService

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,7 +1,34 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+    MessageBody,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+} from '@nestjs/websockets';
 import { ChatService } from './chat.service';
+import { Socket, Server } from 'socket.io';
+import { SendMessageDto } from './dto/send-message.dto';
 
 @WebSocketGateway()
-export class ChatGateway {
-  constructor(private readonly chatService: ChatService) {}
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+    @WebSocketServer() server: Server;
+    constructor(private readonly chatService: ChatService) {}
+
+    handleConnection(client: Socket): void {
+        console.log(`Client connected: ${client.id}`);
+    }
+
+    handleDisconnect(client: Socket): void {
+        console.log(`Client disconnected: ${client.id}`);
+    }
+
+    @SubscribeMessage('send')
+    async handleMessage(@MessageBody() dto: SendMessageDto) {
+        const message = await this.chatService.createMessage(dto);
+
+        this.server.emit('messages', message);
+
+        return message;
+    }
 }
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { SendMessageDto } from './dto/send-message.dto';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async createMessage(dto: SendMessageDto) {
+        const { text } = dto;
+
+        const message = await this.prisma.message.create({
+            data: {
+                text,
+            },
+        });
+
+        return message;
+    }
+}
