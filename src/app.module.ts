ab031954f42265acfefc642478b77a4f2e8172db
Artem Shein <tema34468@gmail.com>
2026-07-10 22:02:37 +0300

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MovieModule } from './movie/movie.module';
 import { ConfigModule } from '@nestjs/config';
-import { ReviewModule } from './review/review.module';
-import { ActorModule } from './actor/actor.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
     imports: [
@@ -13,9 +11,7 @@ import { PrismaModule } from './prisma/prisma.module';
             isGlobal: true,
         }),
         PrismaModule,
-        MovieModule,
-        ReviewModule,
-        ActorModule,
+        ChatModule,
     ],
     controllers: [AppController],
     providers: [AppService],
